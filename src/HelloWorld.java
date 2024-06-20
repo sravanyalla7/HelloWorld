3b93a083b833c47b60d502894db4d1c13d2ac576
sravanyalla7 <sravanreddy7041@gmail.com>
2024-06-20 12:17:09 -0400

    Added few lines of code in HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		
+		//Sample addition
+		int a = 2;
+		int b = 3;
+		System.out.println("Addition is: " +(a+b));
 
 	}
 
